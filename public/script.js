42030dfd065d8e13e4184713f10ad0083a2004ef
Mukesh Rajpurohit <mukeshrr1321@outlook.com>
2021-02-28 06:58:48 +0530

    chat box and video bugs solve

--- public/script.js
+++ public/script.js
@@ -3,44 +3,89 @@ const videoGrid = document.getElementById('video-grid')
 
 const myPeer = new Peer(undefined, {
   host: '/',
-  port: '3001'
+  path:'/peerserver',
+  port: '3000'
 })
+
+
+let Videocontroller;
+let callend;
 
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
+peers.name=prompt("Enter Your Name:-");
+
+
+var getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia;
+
+
+
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
+  Videocontroller=stream
+
   addVideoStream(myVideo, stream)
 
-  myPeer.on('call', call => {
-    call.answer(stream)
+  myPeer.on('call', (call) => {
+    
+
+    call.answer(stream);
+    
     const video = document.createElement('video')
-    call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream)
-    })
+  
+    
+    call.on('stream', (userVideoStream) => {
+      addVideoStream(video, userVideoStream);
+    });
+
   })
 
-  socket.on('user-connected', userId => {
+  socket.on('user-connected', (userId) => {
     connectToNewUser(userId, stream)
   })
-})
+});
+
+
+myPeer.on("call", function (call) {
+  getUserMedia(
+    { video: true, audio: true },
+    function (stream) {
+      call.answer(stream); // Answer the call with an A/V stream.
+      const video = document.createElement("video");
+      call.on("stream", function (remoteStream) {
+        addVideoStream(video, remoteStream);
+      });
+    },
+    function (err) {
+      console.log("Failed to get local stream", err);
+    }
+  );
+});
 
 socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
+  if (peers[userId]) peers[userId].close();
 })
 
-myPeer.on('open', id => {
+myPeer.on('open', (id) => {
   socket.emit('join-room', ROOM_ID, id)
 })
 
-function connectToNewUser(userId, stream) {
+const connectToNewUser=(userId, stream)=>{
 
   const call = myPeer.call(userId, stream)
+
+  callend=call;
+
   const video = document.createElement('video')
-  call.on('stream', userVideoStream => {
+  
+
+  call.on("stream", (userVideoStream) => {
     addVideoStream(video, userVideoStream)
   })
 
@@ -51,10 +96,101 @@ function connectToNewUser(userId, stream) {
   peers[userId] = call
 }
 
+
+
 function addVideoStream(video, stream) {
-  video.srcObject = stream
+  video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
     video.play()
   })
-  videoGrid.append(video)
+  
+  videoGrid.append(video);
+}
+
+const leave=()=>{
+  
+}
+
+
+
+const muteUnmute = () => {
+  const enabled = Videocontroller.getAudioTracks()[0].enabled;
+  if (enabled) {
+    Videocontroller.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    Videocontroller.getAudioTracks()[0].enabled = true;
+  }
+};
+
+
+const StopPlay = () => {
+  let enabled = Videocontroller.getVideoTracks()[0].enabled;
+  if (enabled) {
+    Videocontroller.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    Videocontroller.getVideoTracks()[0].enabled = true;
+  }
+};
+
+
+const setPlayVideo = () => {
+  console.log("pause")
+  const html = `<i class="fa fa-video-slash"></i>`;
+  document.getElementById("playicon").innerHTML = html;
+};
+
+const setStopVideo = () => {
+  const html = `<i class="fa fa-video"></i>`;
+  document.getElementById("playicon").innerHTML = html;
+};
+
+
+const setUnmuteButton = () => {
+  const html = `<i class="fa fa-microphone-slash"></i>`;
+  document.getElementById("muteButton").innerHTML = html;
+};
+const setMuteButton = () => {
+  const html = `<i class="fa fa-microphone"></i>`;
+  document.getElementById("muteButton").innerHTML = html;
+};
+
+
+
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+
+
+socket.emit('new-user', peers.name)
+
+
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+  
+})
+
+socket.on('user-disconnected', name => {
+ 
+})
+
+messageForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const message = messageInput.value
+  appendMessage(`You :  ${message}`)
+  socket.emit('send-chat-message', message)
+  messageInput.value = ''
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
 }
--- server.js
+++ server.js
@@ -1,30 +1,55 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+const io = require("socket.io")(server);
 
+// Peer
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use("/peerserver", peerServer);
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+app.get("/", (req, rsp) => {
+  rsp.redirect(`/${uuidv4()}`);
 
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
+});
+
+const users={}
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.to(roomId).broadcast.emit('user-connected',userId)
 
     socket.on('disconnect', () => {
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
+
   })
+
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+
 })
 
 server.listen(3000)
